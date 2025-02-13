91e68c7ed68e81496e7b097220862d039f577ce2
kevinrodbe <kevinrodbe@users.noreply.github.com>
2025-02-13 15:58:40 -0500

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log(11, process.env);
+console.log('build_env', process.env.build_env);
+console.log('extra_envs', process.env.extra_envs);
+console.log('SET_BUILD_ENV',process.env.SET_BUILD_ENV);
+console.log('SET_SEC_NODE_AUTH_TOKEN', process.env.SET_SEC_NODE_AUTH_TOKEN);
+console.log('SET_SEC_DEPLOY_API_KEY',process.env.SET_SEC_DEPLOY_API_KEY);
+console.log('secret_2', process.env.secret_2);
+console.log('secret_3', process.env.secret_3);
+console.log('secret_4', process.env.secret_4);
